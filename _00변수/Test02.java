75b6ca52f4841c7778280669183b505309f1b145
Leetr <dldnjswo@gmail.com>
2024-08-06 10:50:18 +0900

    00변수 미션 수행 완료

---
+++ _00변수/Test02.java
@@ -0,0 +1,12 @@
+package _00변수;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 오후 문제 풀이
+		// 주요 기능 : 변수, 자료형, 연산자
+		// 문제1
+	}
+
+}
