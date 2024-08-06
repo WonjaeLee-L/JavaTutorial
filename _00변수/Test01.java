2ad38a80ef7614a500b6c23680d1226dc800a854
Leetr <dldnjswo@gmail.com>
2024-08-06 10:36:24 +0900

    00변수 공부

---
+++ _00변수/Test01.java
@@ -0,0 +1,19 @@
+package _00변수;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 정수 변수 선언
+		int a =10;
+		byte aa =10;
+		System.out.println(a);
+		System.out.println(aa);
+		// 소스 실행하는 단축키 : Ctrl + f11
+		
+		// 문자열 변수 선언
+		String s = "Leetr";
+		System.out.println(s);
+	}
+
+}
